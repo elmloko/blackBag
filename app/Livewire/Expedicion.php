419984e28fbd88c495bc20f59195feffa36f1f3c
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-11-11 11:45:53 -0400

    Ventana Expedicion

--- app/Livewire/Expedicion.php
+++ app/Livewire/Expedicion.php
@@ -3,11 +3,34 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Despacho;
+use Carbon\Carbon;
 
 class Expedicion extends Component
 {
+    use WithPagination;
+
+    public $searchTerm = '';
+    public $categoria;
+    public $ofdestino;
+    public $subclase;
+    public $nrodespacho;
+    public $fechaHoraActual;
+    public $perPage = 10;
+
     public function render()
     {
-        return view('livewire.expedicion');
+        $despachos = Despacho::where(function ($query) {
+            $query->where('ofdestino', 'like', '%' . $this->searchTerm . '%')
+                ->orWhere('categoria', 'like', '%' . $this->searchTerm . '%')
+                ->orWhere('subclase', 'like', '%' . $this->searchTerm . '%');
+        })
+            ->whereIn('estado', ['EXPEDICION']) // Filtra solo los estados deseados
+            ->paginate($this->perPage);
+
+        return view('livewire.expedicion', [
+            'despachos' => $despachos,
+        ]);
     }
 }
