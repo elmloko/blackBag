077d0413ef99592d3815d7d5604d0296709bd41f
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-11-15 10:25:35 -0400

    CN38

--- resources/views/despacho/pdf/cn31.blade.php
+++ resources/views/despacho/pdf/cn31.blade.php
@@ -4,9 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CN-35 y CN-31</title>
+    <title>CN-35, CN-31 y CN-38</title>
     <style>
-        /* Estilos para la tabla CN-35 */
+        /* Estilos generales */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            font-size: 12px;
+        }
+
+        th,
+        td {
+            border: 1px solid #000;
+            padding: 5px;
+        }
+
+        thead {
+            background-color: #f2f2f2;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        /* Estilos para CN-35 */
         .cn35-table {
             width: 64%;
             border-collapse: collapse;
@@ -17,10 +42,6 @@
         .cn35-table td {
             border: 1px solid #000;
             padding: 5px;
-        }
-
-        .cn35-table thead {
-            background-color: #f2f2f2;
         }
 
         .transparent-left-border {
@@ -39,25 +60,19 @@
             border-bottom-color: transparent !important;
         }
 
-        .text-center {
-            text-align: center;
-        }
-
-        .text-right {
-            text-align: right;
-        }
-
-        /* Estilos para la tabla CN-31 */
+        /* Estilos para CN-31 */
         .cn31-table {
             width: 100%;
             border-collapse: collapse;
             font-size: 12px;
         }
 
-        .cn31-table th,
-        .cn31-table td {
+        /* Estilos para CN-38 */
+        .first-table th,
+        .first-table td {
             border: 1px solid #000;
             padding: 5px;
+            line-height: 0.5;
         }
 
         .header {
@@ -99,10 +114,166 @@
             font-weight: normal;
             line-height: 0.1;
         }
+
+        /* Footer positioning */
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        .content {
+            flex: 1;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
     </style>
 </head>
 
 <body>
+    <!-- CN-38 Content -->
+    <div class="content">
+        <div class="header">
+            <div class="logo">
+                <img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50">
+                <p class="cn">CN 38</p>
+            </div>
+            <div class="title">
+                <h2>Factura de Entrega</h2>
+                <h3>AGENCIA BOLIVIANA DE CORREOS</h3>
+            </div>
+        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        Oficina de Cambio: {{ $ciudadOrigen }} - {{ $siglaOrigen }}
+                    </th>
+                    <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        Oficina de Destino:{{ $ciudadDestino }} - {{ $ofdestino }}
+                    </th>
+                    <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        @php
+                            $subclaseTranslation = [
+                                'A' => 'Aéreo',
+                                'B' => 'S.A.L.',
+                                'C' => 'Superficie',
+                                'D' => 'Prioritario por superficie',
+                            ];
+                        @endphp
+                        Medio de Transporte: {{ $subclaseTranslation[$categoria] ?? $categoria }}
+                    </th>
+                </tr>
+                <tr>
+                    <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        <p>Fecha: {{ $despacho->created_at->format('Y-m-d') }}</p>
+                    </td>
+                    <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        @php
+                            $claseTranslation = [
+                                'UA' => 'UA CARTAS - AO',
+                                'UB' => 'UB CARTAS - MASIVO',
+                                'UC' => 'UC CARTAS - CORREO DIRECTO ARMONIZADO',
+                                'UD' => 'UD CARTAS - FUERA DEL SISTEMA DE GASTOS TERMINALES',
+                                'UE' => 'UE CARTAS - DECOUVERT',
+                                'UF' => 'UF CARTAS - LC ENTRADA DIRECTA',
+                                'UG' => 'UG CARTAS - AO ENTRADA DIRECTA',
+                                'UH' => 'UH CARTAS - LC/AO ENTRADA DIRECTA',
+                                'UI' => 'UI CARTAS - CCRI',
+                                'UL' => 'UL CARTAS - LC',
+                                'UM' => 'UM CARTAS - SACAS M',
+                                'UN' => 'UN CARTAS - LC/AO',
+                                'UP' => 'UP CARTAS - TARJETAS POSTALES',
+                                'UR' => 'UR CARTAS - CERTIFICADO',
+                                'US' => 'US CARTAS - SACAS VACIAS',
+                                'UT' => 'UT CARTAS - RESERVADO PARA USO DE ACUERDOS BILATERALES',
+                                'UV' =>
+                                    'UV LETTERS - ARTÍCULOS DEVUELTOS QUE NO SE PUEDEN ENTREGAR SUJETOS A REMUNERACIÓN',
+                                'UX' => 'UX CARTAS - EXPRESO',
+                                'UY' => 'UY CARTAS - RESERVADO PARA USO MULTILATERAL EN PROYECTOS DESIGNADOS',
+                                'UZ' => 'UZ CARTAS - RESERVADO PARA USO DE ACUERDOS BILATERALES',
+                            ];
+                        @endphp
+                        Clase: {{ $claseTranslation[$subclase] ?? $subclase }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table class="first-table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Despacho</th>
+                    <th>Origen</th>
+                    <th>Destino</th>
+                    <th>Correspondencia</th>
+                    <th>Peso (Kg.)</th>
+                    <th>Observaciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $i = 1; @endphp
+                @foreach ($sacas as $index => $saca)
+                    <tr>
+                        <td>{{ $i }}</td>
+                        <td>{{ str_pad($despacho->nrodespacho, 3, '0', STR_PAD_LEFT) }} /
+                            {{ str_pad($saca->nrosaca, 3, '0', STR_PAD_LEFT) }}
+                            @if ($index === count($sacas) - 1)
+                                <strong>F</strong>
+                            @endif
+                        </td>
+                        <td>{{ $siglaOrigen }} </td>
+                        <td>{{ $ofdestino }}</td>
+                        <td>{{ $saca->nropaquetes }}</td>
+                        <td>{{ $saca->peso }} Kg.</td>
+                        <td>
+                            @php
+                                $tipos = [
+                                    'BG' => 'BG (Saca)',
+                                    'CG' => 'CG (Rodillo Cesta)',
+                                    'CN' => 'CN (Contenedor)',
+                                    'FW' => 'FW (Carro con bandejas)',
+                                    'GU' => 'GU (Bandeja plana)',
+                                    'IB' => 'IB (Caja en palé IPC)',
+                                    'IL' => 'IL (Bandeja IPC)',
+                                    'IS' => 'IS (Saca de IPC)',
+                                    'PB' => 'PB (Caja en palé)',
+                                    'PU' => 'PU (Bandeja de cartas)',
+                                    'PX' => 'PX (Palé)',
+                                ];
+                                $etiqueta = [
+                                    'RO' => 'RO - Roja',
+                                    'BL' => 'BL - Blanca',
+                                ];
+                            @endphp
+                            {{ $tipos[$saca->tipo] ?? $saca->tipo }}/{{ $etiqueta[$saca->etiqueta] ?? $saca->etiqueta }}
+                        </td>
+                    </tr>
+                    @php $i++; @endphp
+                @endforeach
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <td>SACAS TOTAL</td>
+                    <th>{{ $totalContenido }}</th>
+                    <td>PESO TOTAL</td>
+                    <th>{{ $peso }} Kg.</th>
+                    <td>PAQUETES TOTAL</td>
+                    <th>{{ $totalPaquetes }}</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</body>
+
+<body>
+    <!-- CN-35 Content -->
     @foreach ($sacas as $index => $saca)
         <table class="cn35-table">
             <tr>
@@ -113,14 +284,13 @@
             <tr>
                 <td colspan="2" class="text-center">{{ $ciudadDestino }}</td>
                 <td></td>
-                <!-- Mostrar "F" solo en el último elemento -->
                 @if ($index === count($sacas) - 1)
-                    <h1 class="text-center">F</h1>
+                    <td class="text-center"><strong>F</strong></td>
                 @endif
             </tr>
             <tr>
-                <td>Cat:{{ $categoria }}</td>
-                <td>SubC:{{ $subclase }}</td>
+                <td>Cat: {{ $categoria }}</td>
+                <td>SubC: {{ $subclase }}</td>
                 <td class="transparent-right-border text-right">{{ $siglaOrigen }} (BOA)</td>
                 <td>{{ $ciudadOrigen }} LC/AO</td>
             </tr>
@@ -129,7 +299,6 @@
                 <td colspan="2" class="transparent-bottom-border text-center">AGENCIA BOLIVIANA DE CORREOS</td>
             </tr>
             <tr>
-                
                 <td>DespNo: {{ str_pad($despacho->nrodespacho, 3, '0', STR_PAD_LEFT) }}</td>
                 <td>RecNo: {{ str_pad($saca->nrosaca, 3, '0', STR_PAD_LEFT) }}</td>
                 <td colspan="2" class="text-center">(AGBC)</td>
@@ -166,7 +335,6 @@
             <img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50">
             <p class="cn">CN 31</p>
         </div>
-
         <div class="title">
             <h2>Hoja de Aviso</h2>
             <h3>AGENCIA BOLIVIANA DE CORREOS</h3>
@@ -273,18 +441,18 @@
             </tr>
         </tbody>
     </table>
-    <br>
-    <br>
-    <table class="cn31-table">
-        <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-            <p class="special-text">__________________________</p>
-            <p class="special-text">RECIBIDO POR</p>
-        </td>
-        <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-            <p class="special-text">__________________________ </p>
-            <p class="special-text">ENTREGADO POR</p>
-        </td>
-    </table>
+    <div class="footer">
+        <table>
+            <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                <p class="special-text">__________________________</p>
+                <p class="special-text">RECIBIDO POR</p>
+            </td>
+            <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                <p class="special-text">__________________________ </p>
+                <p class="special-text">ENTREGADO POR</p>
+            </td>
+        </table>
+    </div>
 
 </body>
 
